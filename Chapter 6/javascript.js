7e3dac369072c0d78471ee790e983cd61e0ec7ea
Leewen-Adianah-L-Lumba-101 <144773475+Leewen-Adianah-L-Lumba-101@users.noreply.github.com>
2024-03-19 19:24:41 +0400

    Update javascript.js

--- Chapter 6/javascript.js
+++ Chapter 6/javascript.js
@@ -1,8 +1,10 @@
 function petroltotal() {
-
+    // The variables are named the same as the ids here just to avoid confusion. The parse functions convert the variables into the responding data types (Float, Integer, String etc..)
     var no1 = parseFloat(document.getElementById("no1").value);
     var no2 = parseInt(document.getElementById("no2").value);
 
+    // For some reason, this doesn't work, I don't know why but these act as checks that tell the user a certain field hasn't been given any input.
+    // The only thing that works here is the final else branch.
     if (no1 == "") {
         alert("Please enter the price of petrol per litre.");
 
@@ -18,6 +20,7 @@ function petroltotal() {
     }
 }
 
+// This function is related to clearing the inputs from every field, it also sends an alert of the fields being reset.
 function res(f1) {
     alert("The calculator fields have been reset.");
 }
